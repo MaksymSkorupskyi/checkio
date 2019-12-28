8b81b142d476be3b733393fa0a89e26613125d56
MaxNRG <maxnrg@gmail.com>
2019-12-29 00:17:22 +0200

    assert create_intervals({-1, 0, 1, 12}) == [(-1, 1), (12, 12)]

--- arrays/create_intervals.py
+++ arrays/create_intervals.py
@@ -43,27 +43,28 @@ def create_intervals(data: Set[int]) -> List[Tuple[int, int]]:
     """ Create a list of intervals out of set of ints. """
     starts = []
     ends = []
-    for element in data:
+    for element in sorted(data):
         if element - 1 not in data:
             starts.append(element)
         if element + 1 not in data:
             ends.append(element)
-    return sorted(zip(starts, ends))
+    return list(zip(starts, ends))
 
 
 # v3 - `starts` and `ends` iterators
 def create_intervals(data: Set[int]) -> List[Tuple[int, int]]:
     """ Create a list of intervals out of set of ints. """
+    data = sorted(data)
     starts = (element for element in data if element - 1 not in data)
     ends = (element for element in data if element + 1 not in data)
-    return sorted(zip(starts, ends))
+    return list(zip(starts, ends))
 
 
 # v4 - `starts` and `ends` iterators two-liner
 def create_intervals(data: Set[int]) -> List[Tuple[int, int]]:
     """ Create a list of intervals out of set of ints. """
-    return sorted(zip((element for element in data if element - 1 not in data),
-                      (element for element in data if element + 1 not in data)))
+    return list(zip(sorted(element for element in data if element - 1 not in data),
+                    sorted(element for element in data if element + 1 not in data)))
 
 
 if __name__ == '__main__':
@@ -72,9 +73,11 @@ if __name__ == '__main__':
     print(create_intervals({1, 2, 3, 4, 5, 7, 8, 12}))
     print(create_intervals({4, 5, 7, 8, 12, 4, 20, 17, 1000, 100, 21, 19, 999, 998, 14, 13, 3}))
     print(create_intervals({1, 2, 3, 4, 5, 7, 8, 12}))
+    print(create_intervals({-1, 0, 1, 12}))
 
     # These "asserts" using only for self-checking and not necessary for auto-testing
     assert create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)], "First"
     assert create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)], "Second"
     assert create_intervals({6, 9, 1, 7}) == [(1, 1), (6, 7), (9, 9)], "Third"
+    assert create_intervals({-1, 0, 1, 12}) == [(-1, 1), (12, 12)]
     print('Almost done! The only thing left to do is to Check it!')
